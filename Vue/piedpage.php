c3fab82a0e29055627656204e7d32fe2df089794
textSolver34761 <pradonbenjamin@gmail.com>
2018-03-19 17:36:25 +0100

    correction bug piedpage

--- Vue/piedpage.php
+++ Vue/piedpage.php
@@ -2,11 +2,11 @@
 <div>
     
     <p>
-        Follow me on social networks: <br>
-        <button  class="ui small #B8C7C6 button"> <a href="https://www.twitter.com/benjamin_pradon" target="_blank">Twitter</button> </a>
-        <button  class="ui small #B8C7C6 button"> <a href="https://www.linkedin.com/in/benjaminpradon/" target="_blank">LinkedIn </button> </a>
-        <button  class="ui small #B8C7C6 button"> <a href="https://github.com/textSolver34761" target="_blank"> My Github deposite </button> </a> <br> <br>
-        <button  class="ui small #B8C7C6 button"> <a href="Vue/contact.php">Contact me </button> </a>
+        <h3>Follow me on social networks: </h3> <br>
+        <button  class="ui small #B8C7C6 button"> <a href="https://www.twitter.com/benjamin_pradon" target="_blank">Twitter </a> </button>
+        <button  class="ui small #B8C7C6 button"> <a href="https://www.linkedin.com/in/benjaminpradon/" target="_blank">LinkedIn </a> </button>
+        <button  class="ui small #B8C7C6 button"> <a href="https://github.com/textSolver34761" target="_blank"> My Github deposite </a> </button> <br> <br>
+        <button  class="ui small #B8C7C6 button"> <a href="Vue/contact.php">Contact me </a> </button>
     </p>
     <p>
         &copy; Benjamin Pradon - <?php echo date("Y");?>, all rights reserved
